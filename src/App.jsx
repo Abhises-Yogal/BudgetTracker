1ab5eaa03371d9d9be875ef4882b711a387014d6
Abhises-Yogal <abishekyogal@gmail.com>
2026-06-24 11:31:50 +0545

    feat: Demo app created and basic react component added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function Title(){
+  return (
+    <div>
+    <h1>Hello from my demo site</h1>
+    <p>Use this site for references</p>
+    </div>
+  )
+}
+export default function App(){
+  return (<Title/>)
+}
